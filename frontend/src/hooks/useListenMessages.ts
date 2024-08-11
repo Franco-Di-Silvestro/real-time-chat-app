14ce424987ee27d5650b96e78861553bf9985fd2
Franco-Di-Silvestro <francodisilvestro2000@gmail.com>
2024-08-11 16:56:53 -0300

    Update useListenMessages.ts

--- frontend/src/hooks/useListenMessages.ts
+++ frontend/src/hooks/useListenMessages.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useSocketContext } from '../context/SocketContext'
 import useConversation from '../zustand/useConversation'
-import notificationSound from "../assets/sounds/notification.mp3"
+import notificationSound from "../assets/sounds/notification"
 
 const useListenMessages = () => {
     const { socket } = useSocketContext()
