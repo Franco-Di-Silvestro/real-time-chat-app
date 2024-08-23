52e8307bdb232a6b6250ab21eb93e5e03d50fb4a
Franco-Di-Silvestro <francodisilvestro2000@gmail.com>
2024-08-23 16:29:29 -0300

    prod import bugs

--- frontend/src/hooks/useListenMessages.tsx
+++ frontend/src/hooks/useListenMessages.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSocketContext } from '../context/SocketContext';
 import useConversation from '../zustand/useConversation';
 import notificationSound from "../assets/sounds/notification.mp3";
